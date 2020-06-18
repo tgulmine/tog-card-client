2a7d2a00dead2916b9f0339d82006f0ac81b2c12
tgulmine <tgulmine@gmail.com>
2020-06-17 21:32:40 -0300

    create folders & pick phase

---
+++ src/pages/PickPhase.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './styles/main.scss';
+
+const PickPhase: React.FC = () => {
+  return (
+    <div className="w-full h-screen flex-col bg-red-400">
+      <div className="w-full text-center py-6 text-2xl font-bold text-white">Pick Phase</div>
+    </div>
+  );
+};
+
+export default PickPhase;
